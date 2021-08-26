45187c384cfcb8e08dd442266c21cc4a7467c796
joel shum <joel.shumkwoong@gmail.com>
2021-08-26 12:51:07 +0200

    première insertion d'éléments qui apparaissent au scrolling

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+// Menu Burger animations
+
 let boutonFerme = document.querySelector('.close_button')
 let boutonMenu = document.querySelector('.open_button')
 let menuBurger = document.querySelector('.burger_menu')
@@ -23,3 +25,40 @@ boutonFerme.addEventListener('click', fermer);
 boutonBurger.forEach(bouton => {
     bouton.addEventListener('click', fermer)    
 })
+
+// Animation, apparition scroll progressif
+
+
+// Création d'un observateur d'intersection
+const ratio = .2
+var options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+  }
+
+
+
+  //fonction callback
+
+  const handleIntersect = function (entries, observer) {
+      entries.forEach(entry => {
+          if (entry.intersectionRatio > ratio) {
+            console.log('visible') 
+            entry.target.classList.add('reveal-visible') 
+            observer.unobserve(entry.target)
+          } else {
+              console.log('invisible')
+          }
+          
+      })
+  }
+  
+  const observer = new IntersectionObserver(handleIntersect, options);
+ 
+  // On attribue un élément cible à l'observer, ici classe reveal
+  document.querySelectorAll('.reveal').forEach( r => {
+    observer.observe(r)
+  })
+  
+
